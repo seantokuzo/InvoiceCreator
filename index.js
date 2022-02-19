60aa1bf06abff2d527da4a3c00bad58c299155d7
CrackerJaps <seantokuzo@gmail.com>
2022-02-19 01:51:36 -0800

    map button from services array

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const services = [
     { task: "Do Your Homework", price: 10 },
-    { task: "Do Your Homework", price: 20 },
-    { task: "Do Your Homework", price: 30 }
+    { task: "Take Your SATs", price: 20 },
+    { task: "Falsify Transcripts", price: 30 }
 ]
+const servicesDiv = document.getElementById('services-div')
+const taskList = document.getElementById('task-list')
+const priceList = document.getElementById('price-list')
+const notes = document.getElementById('notes')
+let selectedTasks = []
+let userNotes = ''
+let totalAmount = 0
+
+services.map(taskObj => {
+    const taskButton = document.createElement('button')
+    taskButton.type = 'button'
+    taskButton.textContent = `${taskObj.task}: $${taskObj.price}`
+    taskButton.className = 'service-button'
+    taskButton.onclick = addService
+    servicesDiv.appendChild(taskButton)
+})
+
+function addService() {
+    console.log("add me plz")
+}
